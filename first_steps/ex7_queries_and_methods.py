a06d9cbd9da03b6f3a80c6d16bb59bde2a19a978
d.bibikov <d.bibikov.@trivium.group>
2023-06-21 22:20:41 +0300

    added ex7_queries_and_methods

---
+++ first_steps/ex7_queries_and_methods.py
@@ -0,0 +1,98 @@
+import requests
+from environment import default_url
+
+supported_methods = ['GET', 'POST', 'PUT', 'DELETE']
+
+
+def test_request_without_parameter():
+    response = requests.get(f"{default_url}/compare_query_type")
+    assert str(response.text) == 'Wrong method provided'
+
+    response = requests.post(f"{default_url}/compare_query_type")
+    assert str(response.text) == 'Wrong method provided'
+
+    response = requests.put(f"{default_url}/compare_query_type")
+    assert str(response.text) == 'Wrong method provided'
+
+    response = requests.delete(f"{default_url}/compare_query_type")
+    assert str(response.text) == 'Wrong method provided'
+
+
+def test_no_supported_methods():
+    for method in supported_methods:
+        response = requests.patch(f"{default_url}/compare_query_type", data=f"method={method}")
+        assert response.status_code == 400
+        assert response.text == 'Wrong HTTP method'
+
+    for method in supported_methods:
+        response = requests.head(f"{default_url}/compare_query_type", data=f"method={method}")
+        assert response.status_code == 400
+        print(f"method: {method} response_text: {response.text}")
+        # TODO: при запросе типа head не приходит "Wrong HTTP method"
+        # assert response.text == 'Wrong HTTP method'
+
+
+def test_support_method_and_params():
+    response_get = requests.get(f"{default_url}/compare_query_type", params=f"method=GET")
+    assert response_get.status_code == 200
+    assert response_get.text == '{"success":"!"}'
+
+    response_post = requests.post(f"{default_url}/compare_query_type", data={'method': 'POST'})
+    assert response_post.status_code == 200
+    assert response_post.text == '{"success":"!"}'
+    print(response_post.text)
+
+    response_put = requests.put(f"{default_url}/compare_query_type", data={'method': 'PUT'})
+    assert response_put.status_code == 200
+    assert response_put.text == '{"success":"!"}'
+    print(response_put.text)
+
+
+def test_all_methods_in_params():
+    methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'COPY', 'HEAD', 'OPTIONS',
+               'LINK', 'UNLINK', 'PURGE', 'LOCK', 'UNLOCK', 'PROPFIND', 'VIEW']
+    for method in methods:
+        response_get = requests.get(f"{default_url}/compare_query_type", params=f"method={method}")
+        if method == 'GET':
+            assert response_get.status_code == 200
+            assert response_get.text == '{"success":"!"}'
+        else:
+            assert response_get.status_code == 200
+            assert response_get.text == 'Wrong method provided'
+
+        response_post = requests.post(f"{default_url}/compare_query_type", data={'method': f"{method}"})
+        if method == 'POST':
+            assert response_post.status_code == 200
+            assert response_post.text == '{"success":"!"}'
+        else:
+            assert response_post.status_code == 200
+            assert response_post.text == 'Wrong method provided'
+
+        response_put = requests.put(f"{default_url}/compare_query_type", data={'method': f"{method}"})
+        if method == 'PUT':
+            assert response_put.status_code == 200
+            assert response_put.text == '{"success":"!"}'
+        else:
+            assert response_put.status_code == 200
+            assert response_put.text == 'Wrong method provided'
+
+        response_delete = requests.delete(f"{default_url}/compare_query_type", data={'method': f"{method}"})
+        if method == 'DELETE':
+            assert response_delete.status_code == 200
+            assert response_delete.text == '{"success":"!"}'
+        else:
+            # TODO при запросе с параметром = GET, возвращает success
+            print(f"method: {method} response_text: {response_delete.text}")
+            # assert response_delete.status_code == 200
+            # assert response_delete.text == 'Wrong method provided'
+        response_patch = requests.get(f"{default_url}/compare_query_type", params=f"method={method}")
+        if method == 'PATCH':
+            # TODO при запросе с параметром = PATCH, возвращает  Wrong method provided
+            print(f"method: {method} response_text: {response_delete.text}")
+            assert response_patch.status_code == 200
+            # assert response_patch.text == '{"success":"!"}'
+        else:
+            # TODO при запросе с параметром = GET, возвращает success
+            print(f"method: {method} response_text: {response_delete.text}")
+            assert response_patch.status_code == 200
+            # assert response_patch.text == 'Wrong method provided'
