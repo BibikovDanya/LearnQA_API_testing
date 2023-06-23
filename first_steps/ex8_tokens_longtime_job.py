b6979f4ffb9e37028478c6d124550f372567bd5e
d.bibikov <d.bibikov.@trivium.group>
2023-06-23 18:00:49 +0300

    refactoring ex8_tokens_longtime_job

--- first_steps/ex8_tokens_longtime_job.py
+++ first_steps/ex8_tokens_longtime_job.py
@@ -4,23 +4,35 @@ from environment import default_url
 
 api_path = 'longtime_job'
 
+
 def test_adding_task():
     response_add_task = requests.get(f"{default_url}/longtime_job")
-    token = response_add_task.json()["token"]
-    seconds_to_completion = response_add_task.json()["seconds"]
+    assert response_add_task.status_code == 200, f"Wrong status code: {response_add_task.status_code}"
+    response_dict_add_task = response_add_task.json()
 
-    assert token != []
-    assert seconds_to_completion != []
+    assert "token" in response_dict_add_task, "There is not field 'token' in the response"
+    assert "seconds" in response_dict_add_task, "There is not field 'seconds' in the response"
+    token = response_dict_add_task["token"]
+    seconds_to_completion = response_dict_add_task["seconds"]
 
     response_not_ready_task = requests.get(f"{default_url}/longtime_job", params=f"token={token}")
-    assert response_not_ready_task.json()['status'] == "Job is NOT ready"
+    response_not_ready_task_dict = response_not_ready_task.json()
+    assert 'status' in response_not_ready_task_dict
+    assert response_not_ready_task_dict['status'] == "Job is NOT ready"
 
     time.sleep(seconds_to_completion)
     response_ready_task = requests.get(f"{default_url}/longtime_job", params=f"token={token}")
-    assert response_ready_task.json()['status'] == "Job is ready"
-    assert response_ready_task.json()['result'] != []
-    print(response_ready_task.text)
+    response_ready_task_dict = response_ready_task.json()
+    assert 'status' in response_ready_task_dict, "There is not field 'status' in the response"
+    assert 'result' in response_ready_task_dict, "There is not field 'result' in the response"
+
+    assert response_ready_task_dict['status'] == "Job is ready",\
+        f"Wrong status, expected: {response_ready_task_dict['status']}"
+    assert response_ready_task_dict['result'] != [],\
+        f"Wrong result, expected: {response_ready_task_dict['result']}"
 
     response_no_job_linked = requests.get(f"{default_url}/longtime_job", params="token=123")
-    assert response_no_job_linked.json()['error'] == "No job linked to this token"
-    print(response_no_job_linked.text)
+    response_no_job_linked_dict = response_no_job_linked.json()
+    assert 'error' in response_no_job_linked_dict, "There is not field 'error' in the response"
+    assert response_no_job_linked_dict['error'] == "No job linked to this token", \
+        f"Wrong status, expected: {response_no_job_linked_dict['error']}"
