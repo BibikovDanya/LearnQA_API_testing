a01e8b5fb6494e70687f9f3789c6b93e62a779a1
d.bibikov <d.bibikov.@trivium.group>
2023-06-26 21:04:23 +0300

    added assertion assert_json_has_key

--- lib/assertions.py
+++ lib/assertions.py
@@ -19,3 +19,22 @@ class Assertions:
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        """
+        :param response:
+        :param name: ключ, наличие которого проверяем
+        :return:
+        """
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def expected_status_code(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code} "
