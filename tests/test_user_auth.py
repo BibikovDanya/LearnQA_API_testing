1ebe5857a1a78a74eaf6d2cde38dd11d13de3694
d.bibikov <d.bibikov.@trivium.group>
2023-06-26 21:06:43 +0300

    update setup to setup_method and refactoring

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from environment import default_url
 from lib.base_case import BaseCase
+from lib.assertions import Assertions
 
 
 class TestUserAuth(BaseCase):
@@ -10,7 +11,7 @@ class TestUserAuth(BaseCase):
         ("no_token")
     ]
 
-    def setup(self):
+    def setup_method(self):
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
@@ -23,7 +24,7 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
-    def test_user_auth(self):
+    def test_auth_user(self):
 
         url = f"{default_url}/user/auth"
         response2 = requests.get(
@@ -32,12 +33,12 @@ class TestUserAuth(BaseCase):
             cookies={"auth_sid": self.auth_sid}
 
         )
-
-        assert "user_id" in response2.json(), "There is not user_id in the second response"
-
-        user_id_from_check_method = response2.json()['user_id']
-        assert self.user_id_from_auth_method == user_id_from_check_method, \
-            "User id from auth method is not equal to user id from check method "
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_auth_method,
+            "User id from auth method is not equals to user id from check method"
+        )
 
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
@@ -53,8 +54,10 @@ class TestUserAuth(BaseCase):
                 url,
                 cookies={"auth_sid": self.auth_sid}
             )
-        assert "user_id" in response2.json(), "There is no user id in second response"
 
-        user_id_from_check_method = response2.json()['user_id']
-
-        assert user_id_from_check_method == 0, f"User is authorized witch condition: {condition}"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is authorized witch condition: {condition}"
+        )
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -2,9 +2,30 @@ import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from environment import default_url
+from datetime import datetime
 
 
 class TestUserRegister(BaseCase):
+    def setup_method(self):
+        base_part = 'learnqa'
+        domain = 'example.com'
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_create_user_successfully(self):
+        data = {
+            'password': '1234',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
+
+        response = requests.post(f"{default_url}/user", data=data)
+
+        Assertions.expected_status_code(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = {
@@ -18,6 +39,7 @@ class TestUserRegister(BaseCase):
 
         response = requests.post(f"{default_url}/user", data=data)
 
-        assert response.status_code == 400, f"Received the status code is not 400 but {response.status_code}"
+        Assertions.expected_status_code(response, 400)
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", \
             f"Unexpected response content {response.content}"
+
